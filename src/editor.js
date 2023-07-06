1b4889eab18e9832457cbaf739b24f98318e61a5
TSHKDG <128415318+TSHKDG@users.noreply.github.com>
2023-07-06 16:34:27 +0400

    consoles

--- src/editor.js
+++ src/editor.js
@@ -661,7 +661,15 @@ function createHTML(options = {}) {
             }
             function handleClick(event){
                 var ele = event.target;
-                postAction({type: 'FIND_TABLE', data: (ele.parentNode.parentNode.nodeName === 'TBODY') || (ele.parentNode.parentNode.nodeName === 'TR')});
+                const element = ele.parentNode.parentNode.
+                conat nodeName = element.nodeName
+                if(nodeName === 'TBODY'){
+                    console.log(element.parentNode.getAttribute('id'))
+                }else if(nodeName === 'TR'){
+                    console.log(element.parentNode.parentNode.getAttribute('id'))
+                }
+                
+                postAction({type: 'FIND_TABLE', data: (nodeName === 'TBODY') || (nodeName === 'TR')});
                 if (ele.nodeName === 'INPUT' && ele.type === 'checkbox'){
                     // Set whether the checkbox is selected by default
                     if (ele.checked) ele.setAttribute('checked', '');
