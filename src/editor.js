558defe27fd752322ed127898c588e7058ceac75
TSHKDG <128415318+TSHKDG@users.noreply.github.com>
2023-07-18 09:12:50 +0400

    12

--- src/editor.js
+++ src/editor.js
@@ -693,7 +693,8 @@ function createHTML(options = {}) {
                     postAction({type: 'LINK_TOUCHED', data: ele.getAttribute('data')});
                 }
 
-                if ((ele.nodeName === 'A' || ele.parentNode?.nodeName === 'A')&& ele.getAttribute('href')) {
+                if ((ele.nodeName === 'A' || ele.parentNode?.nodeName === 'A') && ele.getAttribute('href')) {
+                    console.log(ele.parentNode?.nodeName)
                     postAction({type: 'LINK_TOUCHED', data: ele.getAttribute('href') || ele.parentNode?.getAttribute('href')});
                 }
             }
