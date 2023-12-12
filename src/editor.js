425f100f25f794edc56179d6534fa8deade0c745
TSHKDG <128415318+TSHKDG@users.noreply.github.com>
2023-12-12 12:15:52 +0400

    fix again

--- src/editor.js
+++ src/editor.js
@@ -744,7 +744,6 @@ function createHTML(options = {}) {
                     e.preventDefault();
 
                     //getting copied data, and using getData for getting html like string from clipboard
-                    console.log(copiedData?.startsWith('<!DOCTYPE))
                     let copiedData = (e.clipboardData || window.clipboardData).getData("text/html")
                     if(!copiedData?.startsWith('<!DOCTYPE')){
                         copiedData = (e.clipboardData || window.clipboardData).getData("text")
